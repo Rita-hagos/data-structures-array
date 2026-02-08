e4589e98d7c44707c3ac16022d0cad8cbf75db12
Rita-hagos <rhagos2038@sdsu.edu>
2026-02-08 14:14:39 -0800

    Add array size and address output in main function

--- array assignment.cpp
+++ array assignment.cpp
@@ -3,10 +3,13 @@ using namespace std:
 
 int main(){
 
+// code for question number 1
 int arr[100];
 
+//code for question number 2
 cout<<"size of each element:" << sizeof(arr[0]) << "bytes"<<endl;
 
+//code for number 5
 cout << "address of array:" << arr << endl;
 cout << "Address of first element:" <<&arr[0] << endl;
 
